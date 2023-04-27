e8ef586e1d5811d919a21d4073a4f4bd796b3417
Sahithi Pala <43152542+sahithipala1@users.noreply.github.com>
2023-04-27 16:45:09 +0200

    new

---
+++ new.py
@@ -0,0 +1,15 @@
+# Python 2 code for printing
+# on the same line printing
+# geeks and forgetfulness
+# in the same line
+
+print("geeks"),
+print("forgetfulness")
+
+# array
+a = [1, 2, 3, 4]
+
+# printing a element in same
+# line
+for i in range(4):
+    print(a[i])
